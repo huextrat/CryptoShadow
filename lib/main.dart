71b789f3ae56a90672cdd85f1edad5b018286d34
Hugo EXTRAT <extrat.h@gmail.com>
2018-04-01 00:37:20 +0100

    Firebase Messaging

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,17 @@ import 'package:crypto_shadow/ui/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() {
 
-  FirebaseAnalytics analytics = new FirebaseAnalytics();
+  final FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp
   ]);
+
+  _firebaseMessaging.requestNotificationPermissions();
 
   runApp(new MaterialApp(
     title: "CryptoShadow",
